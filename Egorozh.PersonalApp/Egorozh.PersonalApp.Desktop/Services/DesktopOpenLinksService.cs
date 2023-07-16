bd5d05c1b7abac33f401a189687aab16795ed961
zheludkov <zheludkov@ddplanet.ru>
2023-07-16 23:21:31 +0300

    fixed title

--- Egorozh.PersonalApp/Egorozh.PersonalApp.Desktop/Services/DesktopOpenLinksService.cs
+++ Egorozh.PersonalApp/Egorozh.PersonalApp.Desktop/Services/DesktopOpenLinksService.cs
@@ -9,7 +9,7 @@ public class DesktopOpenLinksService : IOpenLinksService
 {
     public void Open(string url)
     {
-        System.Diagnostics.Process.Start(new ProcessStartInfo
+        Process.Start(new ProcessStartInfo
         {
             FileName = url,
             UseShellExecute = true
