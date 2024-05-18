5d2b85bc80d4bfc78c07b6e24388e160f722b756
Егор Желудков <zheludkov@cmsplanet.ru>
2024-05-18 18:31:49 +0300

    feat: changed desktop view

--- flutter/lib/features/home/presentation/pages/desktop_content.dart
+++ flutter/lib/features/home/presentation/pages/desktop_content.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:js' as js;
 
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../gen/assets.gen.dart';
 import '../widgets/widgets.dart';
 
 class DesktopContent extends StatelessWidget {
@@ -9,21 +13,41 @@ class DesktopContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
-        SliverPadding(
-          padding: const EdgeInsets.all(24),
-          sliver: SliverGrid.count(
-            crossAxisSpacing: 24,
-            mainAxisSpacing: 24,
-            crossAxisCount: 6,
-            childAspectRatio: 1.3,
-            children: const [
-              YoutubeTile(),
-              TileTextButton(text: "Обо мне"),
-              TileTextButton(text: "Мои проекты"),
-              TileTextButton(text: "Блог"),
-              TileTextButton(text: "Контакты"),
-              GithubTile(),
-            ],
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Row(
+              children: [
+                TextButton(child: const Text("Обо мне"), onPressed: () {}),
+                TextButton(child: const Text("Мои проекты"), onPressed: () {}),
+                TextButton(child: const Text("Блог"), onPressed: () {}),
+                TextButton(child: const Text("Контакты"), onPressed: () {}),
+                TextButton.icon(
+                  label: const Text("youtube"),
+                  icon: SvgPicture.asset(
+                    Assets.icons.youtube,
+                    colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.secondary, BlendMode.srcIn),
+                    width: 24,
+                    height: 24,
+                  ),
+                  onPressed: () {
+                    js.context.callMethod('open', ['https://www.youtube.com/c/EgorozhCoding']);
+                  },
+                ),
+                TextButton.icon(
+                  label: const Text("github"),
+                  icon: SvgPicture.asset(
+                    Assets.icons.github,
+                    colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.secondary, BlendMode.srcIn),
+                    width: 24,
+                    height: 24,
+                  ),
+                  onPressed: () {
+                    js.context.callMethod('open', ['https://github.com/egorozh']);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
         const SliverToBoxAdapter(
