b64ec6127127e38ea063275326c6f8fce8cb3c82
zheludkov <zheludkov@ddplanet.ru>
2024-05-21 21:38:57 +0300

    feat: fixed navigation

--- flutter/lib/features/home/presentation/pages/scaffold_with_nav_bar.dart
+++ flutter/lib/features/home/presentation/pages/scaffold_with_nav_bar.dart
@@ -1,6 +1,11 @@
-import 'package:egorozh_cv/router/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../../../core/core.dart';
+import '../../../../gen/assets.gen.dart';
+import '../../../../localization.dart';
+import '../../../../router/router.dart';
 
 class ScaffoldWithNavBar extends StatefulWidget {
   const ScaffoldWithNavBar({super.key, required this.child, required this.location});
@@ -13,76 +18,87 @@ class ScaffoldWithNavBar extends StatefulWidget {
 }
 
 class _ScaffoldWithNavBarState extends State<ScaffoldWithNavBar> {
-  int _currentIndex = 0;
-
-  static const List<MyCustomBottomNavBarItem> tabs = [
-    MyCustomBottomNavBarItem(
-      icon: Icon(Icons.home),
-      activeIcon: Icon(Icons.home),
-      label: 'HOME',
-      initialLocation: '/',
-    ),
-    MyCustomBottomNavBarItem(
-      icon: Icon(Icons.explore_outlined),
-      activeIcon: Icon(Icons.explore),
-      label: 'DISCOVER',
-      initialLocation: contactsRoute,
-    ),
-    MyCustomBottomNavBarItem(
-      icon: Icon(Icons.storefront_outlined),
-      activeIcon: Icon(Icons.storefront),
-      label: 'SHOP',
-      initialLocation: blogRoute,
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    const labelStyle = TextStyle(fontFamily: 'Roboto');
+    // const labelStyle = TextStyle(fontFamily: 'Roboto');
     return Scaffold(
-      body: SafeArea(child: widget.child),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedLabelStyle: labelStyle,
-        unselectedLabelStyle: labelStyle,
-        selectedItemColor: const Color(0xFF434343),
-        selectedFontSize: 12,
-        unselectedItemColor: const Color(0xFF838383),
-        showUnselectedLabels: true,
-        type: BottomNavigationBarType.fixed,
-        onTap: (int index) {
-          _goOtherTab(context, index);
-        },
-        currentIndex: widget.location == '/'
-            ? 0
-            : widget.location == '/discover'
-                ? 1
-                : widget.location == '/shop'
-                    ? 2
-                    : 3,
-        items: tabs,
+      body: SafeArea(
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) => [
+            SliverAppBar(
+              title: DesktopAppBar(currentLocation: widget.location),
+              pinned: true,
+            ),
+          ],
+          body: widget.child,
+        ),
       ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   selectedLabelStyle: labelStyle,
+      //   unselectedLabelStyle: labelStyle,
+      //   selectedItemColor: const Color(0xFF434343),
+      //   selectedFontSize: 12,
+      //   unselectedItemColor: const Color(0xFF838383),
+      //   showUnselectedLabels: true,
+      //   type: BottomNavigationBarType.fixed,
+      //   onTap: (int index) {
+      //     _goOtherTab(context, index);
+      //   },
+      //   currentIndex: widget.location == '/'
+      //       ? 0
+      //       : widget.location == contactsRoute
+      //           ? 1
+      //           : widget.location == blogRoute
+      //               ? 2
+      //               : 3,
+      //   items: tabs,
+      // ),
     );
-  }
-
-  void _goOtherTab(BuildContext context, int index) {
-    if (index == _currentIndex) return;
-    GoRouter router = GoRouter.of(context);
-    String location = tabs[index].initialLocation;
-
-    setState(() {
-      _currentIndex = index;
-    });
-    if (index == 3) {
-      context.push('/login');
-    } else {
-      router.go(location);
-    }
   }
 }
 
-class MyCustomBottomNavBarItem extends BottomNavigationBarItem {
-  final String initialLocation;
+class DesktopAppBar extends StatelessWidget {
+  const DesktopAppBar({
+    super.key,
+    required this.currentLocation,
+  });
 
-  const MyCustomBottomNavBarItem({required this.initialLocation, required Widget icon, String? label, Widget? activeIcon})
-      : super(icon: icon, label: label, activeIcon: activeIcon ?? icon);
+  final String currentLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        TextButton(child: Text(context.appTexts.about), onPressed: () => currentLocation == "/" ? null : context.go('/')),
+        TextButton(child: Text(context.appTexts.projects), onPressed: () {}),
+        TextButton(child: Text(context.appTexts.blog), onPressed: () => currentLocation == blogRoute ? null : context.go(blogRoute)),
+        TextButton(
+            child: Text(context.appTexts.contacts), onPressed: () => currentLocation == contactsRoute ? null : context.go(contactsRoute)),
+        TextButton.icon(
+          label: Text(context.appTexts.youtube),
+          icon: SvgPicture.asset(
+            Assets.icons.youtube,
+            colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.secondary, BlendMode.srcIn),
+            width: 24,
+            height: 24,
+          ),
+          onPressed: () {
+            UrlHelper.open(myYoutubeChannelUrl);
+          },
+        ),
+        TextButton.icon(
+          label: Text(context.appTexts.github),
+          icon: SvgPicture.asset(
+            Assets.icons.github,
+            colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.secondary, BlendMode.srcIn),
+            width: 24,
+            height: 24,
+          ),
+          onPressed: () {
+            UrlHelper.open(myGithubUrl);
+          },
+        ),
+      ],
+    );
+  }
 }
