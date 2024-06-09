d19485abfbdfc0e1c0b1523f25c718ad6d2ff56c
zheludkov <zheludkov@ddplanet.ru>
2024-06-09 21:39:02 +0300

    feat: moved switches

--- flutter/lib/features/home/presentation/pages/home_screen.dart
+++ flutter/lib/features/home/presentation/pages/home_screen.dart
@@ -1,10 +1,10 @@
-import 'package:egorozh_cv/localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../core/core.dart';
 import '../../../../gen/assets.gen.dart';
+import '../../../../localization.dart';
 import '../../../../router/router.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -20,9 +20,19 @@ class HomeScreen extends StatelessWidget {
         final screenType = AdaptiveHelper.getScreenType(context);
 
         return Scaffold(
-          appBar: screenType == ScreenType.desktop ? AppBar(title: _DesktopAppBar(location)) : AppBar(),
+          appBar: screenType == ScreenType.desktop
+              ? AppBar(
+                  title: _DesktopAppBar(location),
+                  leadingWidth: 24.0 + 48,
+                  leading: Padding(
+                    padding: const EdgeInsets.only(left: 24.0, right: 8.0),
+                    child: CircleAvatar(child: ClipOval(child: Assets.splash.image(fit: BoxFit.fitHeight))),
+                  ),
+                )
+              : AppBar(),
           body: child,
           drawer: screenType != ScreenType.desktop ? _MobileDrawer(currentLocation: location) : null,
+          bottomSheet: screenType == ScreenType.desktop ? const Padding(padding: EdgeInsets.all(8.0), child: _PoweredBy()) : null,
         );
       },
     );
@@ -84,44 +94,6 @@ class _DesktopAppBar extends StatelessWidget {
   }
 }
 
-class _MobileNavBar extends StatelessWidget {
-  const _MobileNavBar(this.currentLocation);
-
-  final String currentLocation;
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      useLegacyColorScheme: false,
-      showSelectedLabels: true,
-      showUnselectedLabels: true,
-      unselectedItemColor: Theme.of(context).colorScheme.onSurface,
-      currentIndex: switch (currentLocation) {
-        Routes.homeRoute => 0,
-        Routes.blogRoute => 1,
-        Routes.projectsRoute => 2,
-        Routes.contactsRoute => 3,
-        _ => 0,
-      },
-      items: [
-        BottomNavigationBarItem(icon: const Icon(Icons.home), label: context.appTexts.about),
-        BottomNavigationBarItem(icon: const Icon(Icons.book), label: context.appTexts.blog),
-        BottomNavigationBarItem(icon: const Icon(Icons.work), label: context.appTexts.projects),
-        BottomNavigationBarItem(icon: const Icon(Icons.contacts), label: context.appTexts.contacts),
-      ],
-      onTap: (index) => context.go(
-        switch (index) {
-          0 => Routes.homeRoute,
-          1 => Routes.blogRoute,
-          2 => Routes.projectsRoute,
-          3 => Routes.contactsRoute,
-          _ => Routes.homeRoute,
-        },
-      ),
-    );
-  }
-}
-
 class _MobileDrawer extends StatelessWidget {
   const _MobileDrawer({required this.currentLocation});
 
@@ -132,67 +104,73 @@ class _MobileDrawer extends StatelessWidget {
     return Drawer(
       child: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            DrawerHeader(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    child: CircleAvatar(child: ClipOval(child: Assets.splash.image(fit: BoxFit.fitHeight))),
-                  ),
-                  Text(context.appTexts.title, textAlign: TextAlign.center),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              DrawerHeader(
+                padding: const EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 8.0),
+                child: Stack(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          child: CircleAvatar(child: ClipOval(child: Assets.splash.image(fit: BoxFit.fitHeight))),
+                        ),
+                        Text(context.appTexts.title, textAlign: TextAlign.center),
+                      ],
+                    ),
+                    const Align(alignment: Alignment.topLeft, child: ThemeSwitch()),
+                    const Align(alignment: Alignment.topRight, child: LangSwitch()),
+                  ],
+                ),
               ),
-            ),
-            TextButton(
-              onPressed: currentLocation == Routes.homeRoute ? null : () => navigateAndCloseDrawer(context, Routes.homeRoute),
-              child: Text(context.appTexts.about),
-            ),
-            const SizedBox(height: 24),
-            TextButton(
-              onPressed: currentLocation == Routes.projectsRoute ? null : () => navigateAndCloseDrawer(context, Routes.projectsRoute),
-              child: Text(context.appTexts.projects),
-            ),
-            const SizedBox(height: 24),
-            TextButton(
-              onPressed: currentLocation == Routes.blogRoute ? null : () => navigateAndCloseDrawer(context, Routes.blogRoute),
-              child: Text(context.appTexts.blog),
-            ),
-            const SizedBox(height: 24),
-            TextButton(
-              onPressed: currentLocation == Routes.contactsRoute ? null : () => navigateAndCloseDrawer(context, Routes.contactsRoute),
-              child: Text(context.appTexts.contacts),
-            ),
-            const SizedBox(height: 24),
-            TextButton.icon(
-              label: Text(context.appTexts.youtube),
-              icon: SvgPicture.asset(
-                Assets.icons.youtube,
-                colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.secondary, BlendMode.srcIn),
-                width: 24,
-                height: 24,
+              TextButton(
+                onPressed: currentLocation == Routes.homeRoute ? null : () => navigateAndCloseDrawer(context, Routes.homeRoute),
+                child: Text(context.appTexts.about),
               ),
-              onPressed: () => UrlHelper.open(myYoutubeChannelUrl),
-            ),
-            const SizedBox(height: 24),
-            TextButton.icon(
-              label: Text(context.appTexts.github),
-              icon: SvgPicture.asset(
-                Assets.icons.github,
-                colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.secondary, BlendMode.srcIn),
-                width: 24,
-                height: 24,
+              const SizedBox(height: 24),
+              TextButton(
+                onPressed: currentLocation == Routes.projectsRoute ? null : () => navigateAndCloseDrawer(context, Routes.projectsRoute),
+                child: Text(context.appTexts.projects),
               ),
-              onPressed: () => UrlHelper.open(myGithubUrl),
-            ),
-            const Spacer(),
-            const ThemeSwitch(),
-            const SizedBox(height: 24),
-            const LangSwitch(),
-            const SizedBox(height: 24),
-          ],
+              const SizedBox(height: 24),
+              TextButton(
+                onPressed: currentLocation == Routes.blogRoute ? null : () => navigateAndCloseDrawer(context, Routes.blogRoute),
+                child: Text(context.appTexts.blog),
+              ),
+              const SizedBox(height: 24),
+              TextButton(
+                onPressed: currentLocation == Routes.contactsRoute ? null : () => navigateAndCloseDrawer(context, Routes.contactsRoute),
+                child: Text(context.appTexts.contacts),
+              ),
+              const SizedBox(height: 24),
+              TextButton.icon(
+                label: Text(context.appTexts.youtube),
+                icon: SvgPicture.asset(
+                  Assets.icons.youtube,
+                  colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.secondary, BlendMode.srcIn),
+                  width: 24,
+                  height: 24,
+                ),
+                onPressed: () => UrlHelper.open(myYoutubeChannelUrl),
+              ),
+              const SizedBox(height: 24),
+              TextButton.icon(
+                label: Text(context.appTexts.github),
+                icon: SvgPicture.asset(
+                  Assets.icons.github,
+                  colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.secondary, BlendMode.srcIn),
+                  width: 24,
+                  height: 24,
+                ),
+                onPressed: () => UrlHelper.open(myGithubUrl),
+              ),
+              const SizedBox(height: 48),
+              const _PoweredBy(),
+            ],
+          ),
         ),
       ),
     );
@@ -202,5 +180,18 @@ class _MobileDrawer extends StatelessWidget {
     context.go(route);
 
     Navigator.pop(context);
+  }
+}
+
+class _PoweredBy extends StatelessWidget {
+  const _PoweredBy();
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      context.appTexts.powered_by,
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.labelMedium,
+    );
   }
 }
